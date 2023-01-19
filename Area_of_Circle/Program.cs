c2904c527cfd5d5519920272756d0b72e53061e4
NS0087 <nitinsaraswat.30@gmail.com>
2023-01-19 18:26:29 +0530

    area of circle

---
+++ Area_of_Circle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_of_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.Write("Enter the radius = ");
+                int r = Convert.ToInt32(Console.ReadLine());
+
+                double area = 3.14 * r * r;
+                Console.WriteLine("Area of Circle is = " + area);
+                Console.ReadKey();
+        }
+    }
+}
