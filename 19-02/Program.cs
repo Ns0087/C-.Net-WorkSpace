cf2690b8653d045dc7b0ce556858ecd5e7876c63
NS0087 <nitinsaraswat.30@gmail.com>
2023-01-19 16:41:42 +0530

    19-01-2023

---
+++ 19-02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _19_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = a * b;
+
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
