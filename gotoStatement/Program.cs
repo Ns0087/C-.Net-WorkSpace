abcadb8140f1aa39c8b82681a1800c1d36f33d87
NS0087 <nitinsaraswat.30@gmail.com>
2023-01-19 18:39:33 +0530

    goto statement

---
+++ gotoStatement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gotoStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("One");
+            Console.WriteLine("Two");
+            goto mylabel;
+            Console.WriteLine("Three");
+            mylabel:
+            Console.WriteLine("Four");
+            Console.WriteLine("Five");
+
+            Console.ReadKey();
+        }
+    }
+}
