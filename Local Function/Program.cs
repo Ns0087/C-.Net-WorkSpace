67d347aa63f35a2f8865cfd8b6cf00ce035e1b2e
NS0087 <nitinsaraswat.30@gmail.com>
2023-01-20 18:11:08 +0530

    20th jan 2023 till 06:11

---
+++ Local Function/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Local_Function
+{
+    class Program
+    {
+        public void displayMarks(int m1, int m2, int m3)
+        {
+            double avgMarks = getAverage();
+            Console.WriteLine("Marks 1 : " + m1);
+            Console.WriteLine("Marks 2 : " + m2);
+            Console.WriteLine("Marks 1 : " + m1);
+            Console.WriteLine("Average Marks : " + avgMarks);
+
+            double getAverage()
+            {
+                double avg;
+                avg = (double)(m1 + m2 + m3)/3;
+                return avg;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+
+            obj.displayMarks(80, 85, 78);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TryParseMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParseMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "1001";
+            string str2 = "100 1";
+            bool flag1 = int.TryParse(str1, out int b);
+            Console.WriteLine(flag1);
+            bool flag2 = int.TryParse(str2, out int c);
+            Console.WriteLine(flag2);
+
+            Console.ReadKey();
+        }
+    }
+}
