e98c19842e4ed5d7a46cc5cade2bcdb9f71a9074
NS0087 <nitinsaraswat.30@gmail.com>
2023-01-24 19:01:39 +0530

    24th Jan till 07:00 PM

---
+++ Pattern Matching Example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pattern_Matching_Library;
+
+namespace Pattern_Matching_Example
+{
+    class Program
+    {
+        static void Main()
+        {
+            //refernce Variable
+            ParentClass pc;
+
+            //Object of child class
+            pc = new ChildClass() { x = 10, y = 20 };
+
+            //access parent class's members
+            Console.WriteLine(pc.x);
+            if(pc is ChildClass cc)
+            {
+                Console.WriteLine(cc.y);
+            }
+            Console.ReadKey();
+        }
+    }
+}
