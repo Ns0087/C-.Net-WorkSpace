29e551cfb1ebdc22f3cd8594e25aa0aac020d1eb
Nitin <nitin.saraswat@remotestate.com>
2023-01-25 19:35:40 +0530

    25th jan till 07:40

---
+++ Pattern Matching Library/ParentClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Matching_Library
+{
+    public class ParentClass
+    {
+        public int x { get; set; }
+    }
+
+    public class ChildClass : ParentClass
+    {
+        public int y { get; set; }
+    }
+}
