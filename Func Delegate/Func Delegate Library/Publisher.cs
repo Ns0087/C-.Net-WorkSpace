1bd974fa483a5dd8cf7b323137677a214d5f24bd
Nitin <nitin.saraswat@remotestate.com>
2023-01-26 22:46:38 +0530

    26th Jan till 10:46

---
+++ Func Delegate/Func Delegate Library/Publisher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Func_Delegate_Library
+{
+
+    public class Publisher
+    {
+        public event Func<int, int, int> myEvent;
+
+        public int RaiseEvent(int a, int b)
+        {
+            if (myEvent != null) { return myEvent(a, b); }
+            return 0;
+        }
+    }
+}
---
+++ Func Delegate/Func Delegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Func_Delegate_Library;
+
+namespace Func_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher publisher = new Publisher();
+
+            publisher.myEvent += (a, b) => a + b;
+
+            Console.WriteLine(publisher.RaiseEvent(10, 22));
+
+            Console.ReadKey();
+        }
+    }
+}
