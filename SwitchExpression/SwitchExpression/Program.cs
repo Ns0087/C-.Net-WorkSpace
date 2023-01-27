7ca33af0b689117d930ecc42554c93d18f36e0f8
Nitin <nitin.saraswat@remotestate.com>
2023-01-27 11:57:37 +0530

    27th Jan till 11:57 AM

---
+++ SwitchExpression/SwitchExpression/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int operation = int.Parse(Console.ReadLine());
+            string result;
+
+            result = operation switch
+            {
+                1 => "Customer",
+                2 => "Employee",
+                3 => "Supplier",
+                4 => "Distributor",
+                _ => "No Case Available"
+            };
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
