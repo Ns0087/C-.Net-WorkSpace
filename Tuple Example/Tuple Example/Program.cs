0ba8d1d1ab130678156a582eaf3b309ff1bfa319
Nitin <nitin.saraswat@remotestate.com>
2023-01-30 13:51:34 +0530

    30th Jan till 1:50 PM

---
+++ Tuple Example/Tuple Example/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuple_Example
+{
+    public class TupleExample
+    {
+        public Tuple<string, int> GetPersonDetails()
+        {
+            string Name = "Nitin";
+            int age = 23;
+            Tuple<string, int> tuple = new Tuple<string, int>(Name, age);
+            return tuple;
+        }
+
+        //Value Tuple
+        public (int custId, string custName, int age) GetCustomerDetails()
+        {
+            return (1001, "Aaryan", 24);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TupleExample example = new TupleExample();
+            Tuple<string, int> person = example.GetPersonDetails();
+
+            //Value Tuple
+            var customer1 = example.GetCustomerDetails();
+
+            Console.WriteLine(customer1.custId+ " " + customer1.custName+ " " + customer1.age);
+
+            //Deconstructing
+            (int cId, string cName, int age) = example.GetCustomerDetails();
+
+            Console.WriteLine(cId);
+            Console.WriteLine(cName);
+            Console.WriteLine(age);
+
+            //Discard
+            (int custId, string custName, _) = example.GetCustomerDetails();
+
+            Console.WriteLine(custId);
+            Console.WriteLine(custName);
+
+            //access value from tuple
+            Console.WriteLine(person.Item1);
+            Console.WriteLine(person.Item2);
+
+            Console.ReadKey();
+        }
+    }
+}
