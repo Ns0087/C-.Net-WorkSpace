4fa8f30b415843fdd6015ab4d471bae0219b8901
Nitin <nitin.saraswat@remotestate.com>
2023-01-30 18:54:11 +0530

    30th Jan till 06:54 PM

---
+++ Regular+Expressions/RegExpExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegExpExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Regex regex = new Regex("^[A-Za-z ]*$");
+            Console.WriteLine("Enter a name: ");
+            string inputValue;
+            bool result = false;
+
+            do
+            {
+                inputValue = Console.ReadLine();
+                result = regex.IsMatch(inputValue);
+                Console.WriteLine(result);
+                if (result == false)
+                    Console.WriteLine("Please Enter a Valid Name!!!");
+
+            } while (result != true);
+            //if (result == true)
+            //{
+            //    Console.WriteLine("Valid name");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid name");
+            //}
+            Console.ReadKey();
+        }
+    }
+}
